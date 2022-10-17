76ab2fb0d1b0dc7a213acd7baaecc604cbf8c04a
a-programmer <mrsadin@gmail.com>
2022-10-17 22:20:17 +0330

    Errors enum improvement

--- src/Enums/ApiResultStatusCode.cs
+++ src/Enums/ApiResultStatusCode.cs
@@ -5,31 +5,31 @@ namespace KSFramework.Enums
 {
     public enum ApiResultStatusCode
     {
-        [Display(Name = "عملیات با موفقیت انجام شد")]
+        [Display(Name = "Successfull")]
         Success = 200,
 
-        [Display(Name = "خطایی در سرور رخ داده است")]
+        [Display(Name = "Server Error")]
         ServerError = 500,
 
-        [Display(Name = "پارامتر های ارسالی معتبر نیستند")]
+        [Display(Name = "Bad Request")]
         BadRequest = 400,
 
-        [Display(Name = "یافت نشد")]
+        [Display(Name = "Not Found")]
         NotFound = 404,
 
-        [Display(Name = "لیست خالی است")]
+        [Display(Name = "Empty List")]
         ListEmpty = 404,
 
-        [Display(Name = "خطایی در پردازش رخ داد")]
+        [Display(Name = "Processing Error")]
         LogicError = 500,
 
-        [Display(Name = "داده ای با این مشخصات موجود می باشد")]
+        [Display(Name = "Record Exists")]
         ExistingRecord = 501,
 
-        [Display(Name = "خطای احراز هویت")]
+        [Display(Name = "Unauthorized Access")]
         UnAuthorized = 403,
 
-        [Display(Name = "خطا در احراز هویت")]
+        [Display(Name = "Authentication Error")]
         AuthenticationFailed = 401
     }
 }
