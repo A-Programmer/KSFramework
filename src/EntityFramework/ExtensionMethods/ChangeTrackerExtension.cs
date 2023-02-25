740b2f738a066a6b24b579e66daf9a71a08189a4
a-programmer <mrsadin@gmail.com>
2023-02-25 18:59:42 +0330

    Fixing AggregateRoot issue on soft delete

--- src/EntityFramework/ExtensionMethods/ChangeTrackerExtension.cs
+++ src/EntityFramework/ExtensionMethods/ChangeTrackerExtension.cs
@@ -9,16 +9,34 @@ public static class ChangeTrackerExtensions
     public static void SetAuditProperties(this ChangeTracker changeTracker)
     {
         changeTracker.DetectChanges();
+        
         IEnumerable<EntityEntry> entities =
             changeTracker
                 .Entries()
                 .Where(t => t.Entity is EntityWithSoftDelete && t.State == EntityState.Deleted);
+        
+        IEnumerable<EntityEntry> aggregateRoots =
+            changeTracker
+                .Entries()
+                .Where(t => t.Entity is AggregateRootWithSoftDelete && t.State == EntityState.Deleted);
 
-        if (entities.Any())
+        var entityEntriesArray = entities as EntityEntry[] ?? entities.ToArray();
+        if (entityEntriesArray.Any())
         {
-            foreach(EntityEntry entry in entities)
+            foreach(EntityEntry entry in entityEntriesArray)
             {
                 EntityWithSoftDelete entity = (EntityWithSoftDelete)entry.Entity;
+                entity.Delete();
+                entry.State = EntityState.Modified;
+            }
+        }
+
+        var aggregateRootsArray = aggregateRoots as EntityEntry[] ?? aggregateRoots.ToArray();
+        if (aggregateRootsArray.Any())
+        {
+            foreach(EntityEntry entry in aggregateRootsArray)
+            {
+                AggregateRootWithSoftDelete entity = (AggregateRootWithSoftDelete)entry.Entity;
                 entity.Delete();
                 entry.State = EntityState.Modified;
             }
