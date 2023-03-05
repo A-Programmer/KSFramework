41beea5bf6c5f0ea9f5f678c3446295b2b6ce3ac
a-programmer <mrsadin@gmail.com>
2023-03-05 08:44:01 +0330

    SoftDelete-21

--- src/Primitives/ISoftDelete.cs
+++ src/Primitives/ISoftDelete.cs
@@ -3,6 +3,7 @@ namespace KSFramework.Primitives;
 public interface ISoftDelete
 {
     public bool IsDeleted { get; set; }
+    public DateTimeOffset DeletedAt { get; set; }
     public void Delete();
     public void Recover();
 }
