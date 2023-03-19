07794817dedfebf5265e92d99fd00b1da8776abd
a-programmer <mrsadin@gmail.com>
2023-03-19 14:12:49 +0330

    Removing Logical delete feature

--- src/EntityFramework/Maps/Base/ISoftDeleteMapConfiguration.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace KSFramework.EntityFramework.Maps.Base;
-
-public interface ISoftDeleteMapConfiguration<TWithSoftDelete>
-    : IEntityTypeConfiguration<TWithSoftDelete> where TWithSoftDelete : class
-{
-
-}
--- src/Primitives/ISoftDelete.cs
+++
@@ -1,9 +0,0 @@
-namespace KSFramework.Primitives;
-
-public interface ISoftDelete
-{
-    public bool IsDeleted { get; set; }
-    public DateTimeOffset? DeletedAt { get; set; }
-    public void Delete();
-    public void Recover();
-}
