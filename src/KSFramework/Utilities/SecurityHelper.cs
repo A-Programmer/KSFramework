07a590f89bab7ef2930b7153b0132c136ed1ea61
Mohamadreza Nakhleh <neo.vortex@pm.me>
2025-06-11 11:01:21 +0330

    improve: CA1850: Prefer static HashData method over ComputeHash

--- src/KSFramework/Utilities/SecurityHelper.cs
+++ src/KSFramework/Utilities/SecurityHelper.cs
@@ -7,9 +7,8 @@ public static class SecurityHelper
     public static string GetSha256Hash(string input)
     {
         //using (var sha256 = new SHA256CryptoServiceProvider())
-        using var sha256 = SHA256.Create();
         var byteValue = Encoding.UTF8.GetBytes(input);
-        var byteHash = sha256.ComputeHash(byteValue);
+        var byteHash = SHA256.HashData(byteValue);
         return Convert.ToBase64String(byteHash);
         //return BitConverter.ToString(byteHash).Replace("-", "").ToLower();
     }
