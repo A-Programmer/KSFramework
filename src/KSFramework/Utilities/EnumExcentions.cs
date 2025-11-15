01f9f58874022c3673b5c1f181b7649879183da1
Kamran Sadin <MrSadin@Gmail.Com>
2025-11-15 19:01:31 +0330

    Add ToDescription extension method for Enum

--- src/KSFramework/Utilities/EnumExcentions.cs
+++ src/KSFramework/Utilities/EnumExcentions.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace KSFramework.Utilities;
@@ -39,6 +40,41 @@ public static class EnumExtensions
     public static Dictionary<int, string> ToDictionary(this Enum value)
     {
         return Enum.GetValues(value.GetType()).Cast<Enum>().ToDictionary(p => Convert.ToInt32(p), q => ToDisplay(q));
+    }
+
+    public static Dictionary<int, string> ToDescriptionDictionary(this Enum value)
+    {
+        return Enum.GetValues(value.GetType()).Cast<Enum>().ToDictionary(p => Convert.ToInt32(p), q => ToDisplay(q));
+    }
+    
+    public static Dictionary<string, string> GetEnumDescriptions<TEnum>() where TEnum : struct, Enum
+    {
+        var dict = new Dictionary<string, string>();
+        foreach (var value in Enum.GetValues<TEnum>())
+        {
+            var field = typeof(TEnum).GetField(value.ToString());
+            var descAttr = field?.GetCustomAttribute<DescriptionAttribute>();
+            dict.Add(value.ToString(), descAttr?.Description ?? value.ToString());
+        }
+        return dict;
+    }
+
+    public static Dictionary<int, string> ToDictionary<TEnum>() where TEnum : Enum
+    {
+        return Enum.GetValues(typeof(TEnum))
+            .Cast<TEnum>()
+            .ToDictionary(
+                value => Convert.ToInt32(value),
+                value => value.GetDescription()
+            );
+    }
+    
+    private static string GetDescription<TEnum>(this TEnum value)
+    {
+        FieldInfo field = value.GetType().GetField(value.ToString());
+        var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+
+        return attribute?.Description ?? value.ToString();
     }
 }
 
